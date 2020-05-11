ff6765863b119e0b17405520a759b89e4b8a0fab
José Rafael Morente González <josemorenteg98@outlook.com>
2020-05-11 08:15:28 -0600

    [DELETED]
    Eliminado backend con typescript.

--- back-olc121/dist/jison/analizador-lexico.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
-var JisonLex = require('jison-lex');
-var symbols = fs.readFileSync('./lexico.jison', 'utf8');
-var lexer = new JisonLex(symbols);
-module.exports = lexer;
--- back-olc121/src/jison/analizador-lexico.ts
+++
@@ -1,7 +0,0 @@
-import fs = require('fs');
-var JisonLex = require('jison-lex');
-
-var symbols = fs.readFileSync('./lexico.jison', 'utf8');
-var lexer = new JisonLex(symbols);
-
-module.exports = lexer;
